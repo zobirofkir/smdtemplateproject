bcaa67dabcc33f681a617d8c57ceda15085775f2
zobirofkir <zobirofkir19@gmail.com>
2024-11-22 11:19:29 +0100

    style slider

--- resources/views/components/abouts.blade.php
+++ resources/views/components/abouts.blade.php
@@ -8,15 +8,20 @@
                 <div class="about-content text-center mb-55">
                     <div class="section-title mb-30">
                         <span class="wow fadeInDown" data-wow-delay=".2s">À propos de nous</span>
-                        <h2 class="mb-15 wow fadeInUp" data-wow-delay=".4s">Bienvenue dans notre clinique</h2>
+                        <h2 class="mb-15 wow fadeInUp" data-wow-delay=".4s">Diabète au Maroc </h2>
                     </div>
-                    <p class="mb-35 wow fadeInUp" data-wow-delay=".6s">Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-                        sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero
-                        eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
-                        gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-                        sadipscing elitr,
-                        sed diam nonumy eirmod tempor invidunt.</p>
-                    <a href="#" class="btn theme-btn wow fadeInUp" data-wow-delay=".8s">En savoir plus</a>
+                    <p class="mb-35 wow fadeInUp" data-wow-delay=".6s">
+                        Une Révolution Éclairée par la Société Marocaine de Diabétologie
+                    </p>
+
+                    <p class="mb-35 wow fadeInUp" data-wow-delay=".6s">
+                        Le diabète est une maladie chronique qui touche de plus en plus de Marocains. Ce Site explore les initiatives de la Société Marocaine de Diabétologie pour sensibiliser et éduquer la population sur cette affection. Ensemble, nous allons découvrir les enjeux et les solutions proposées.
+                    </p>
+
+                    <p class="mb-35 wow fadeInUp" data-wow-delay=".6s">
+                        Au Maroc, plus de 3 millions de personnes vivent avec le diabète. Les chiffres continuent d'augmenter, ce qui souligne l'importance d'une prévention efficace et d'une éducation appropriée. Comprendre ces statistiques est crucial pour agir rapidement.
+                    </p>
+
                 </div>
             </div>
         </div>
@@ -24,4 +29,34 @@
     <div class="about-img text-center">
         <img src="assets/img/about/about-img.png" style="border-radius: 20px" alt="">
     </div>
+
+    <div class="container my-20">
+        <div class="row">
+            <div class="col-xl-10 col-lg-11 mx-auto">
+                <p class="mb-35 wow fadeInUp text-center" data-wow-delay=".6s">
+                    La Société Marocaine de Diabétologie a lancé plusieurs programmes de sensibilisation pour informer le public sur les risques liés au diabète. Ces initiatives incluent des ateliers, des conférences et des campagnes médiatiques qui touchent un large public.
+                </p>
+
+                <p class="mb-35 wow fadeInUp text-center" data-wow-delay=".6s">
+                    Importance de l'Éducation
+                    Éduquer les patients sur la gestion du diabète est essentiel. La Société Marocaine de Diabétologie propose des ressources et des formations pour aider les patients à comprendre leur condition et à prendre des décisions éclairées sur leur santé.
+                </p>
+
+                <p class="mb-35 wow fadeInUp text-center" data-wow-delay=".6s">
+                    Témoignages Inspirants
+                    Des témoignages de patients qui ont surmonté les défis du diabète montrent la résilience et la détermination. Ces histoires encouragent d'autres à adopter des modes de vie sains et à participer aux programmes offerts par la société.
+                </p>
+
+                <p class="mb-35 wow fadeInUp text-center" data-wow-delay=".6s">
+                    le combat contre le diabète au Maroc nécessite une collaboration entre les professionnels de santé et la société. Avec l'engagement de la Société Marocaine de Diabétologie, nous pouvons espérer un avenir où le diabète est mieux géré et compris.
+                </p>
+
+                <div class="text-center">
+                    <a href="#" class="btn theme-btn wow fadeInUp text-center" data-wow-delay=".8s">En savoir plus</a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
 </section>
--- resources/views/components/slider.blade.php
+++ resources/views/components/slider.blade.php
@@ -2,58 +2,79 @@
 
     <section class="slider-section">
         <div class="slider-active slick-style">
-            <div class="single-slider img-bg" style="background-image:url('assets/img/slider/slider-1.jpg');">
+            <div class="single-slider img-bg" style="background: rgb(188, 255, 188)">
                 <div class="container">
                     <div class="row">
-                        <div class="col-xl-7 col-lg-8 col-md-10">
+                        <div class="d-flex align-items-center justify-content-between w-100 col-xl-7 col-lg-8 col-md-10">
                             <div class="slider-content">
                                 <h1 data-animation="fadeInDown" data-duration="1.5s" data-delay=".5s">
-                                    {{config('app.name')}}
+                                    Société Morocaine
+                                    <br>
+                                    <b>de Diabète</b>
                                 </h1>
-                                <p data-animation="fadeInLeft" data-duration="1.5s" data-delay=".7s">Lorem ipsum
-                                    dolor sit amet, consetetur sadipscing elitr, sed dinonumy eirmod tempor invidunt
-                                    ut labore et dolore magna aliquyam erat. Lorem ipsum dolor sit amet, consetetur
-                                    sadipscing elitr.</p>
-                                <a href="#about" class="btn theme-btn page-scroll" data-animation="fadeInUp" data-duration="1.5s"
-                                    data-delay=".9s">En savoir plus</a>
+                                <h2 data-animation="fadeInDown" data-duration="1.5s" data-delay=".5s">
+                                    Moroccan Diabetes Society
+                                </h2>
+
+                                <h3 data-animation="fadeInDown" data-duration="1.5s" data-delay=".5s">
+                                    الجمعية المغربية لمرض السكري
+                                </h3>
+                            </div>
+
+                            <div>
+                                <img src="{{asset('assets/img/logo/logo.png')}}" alt="">
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="single-slider img-bg" style="background-image:url('assets/img/slider/slider-2.jpg');">
+            <div class="single-slider img-bg" style="background: rgb(188, 255, 188)">
                 <div class="container">
                     <div class="row">
-                        <div class="col-xl-7 col-lg-8 col-md-10">
+                        <div class="d-flex align-items-center justify-content-between w-100 col-xl-7 col-lg-8 col-md-10">
                             <div class="slider-content">
                                 <h1 data-animation="fadeInDown" data-duration="1.5s" data-delay=".5s">
-                                    {{config('app.name')}}
+                                    Société Morocaine
+                                    <br>
+                                    <b>de Diabète</b>
                                 </h1>
-                                <p data-animation="fadeInLeft" data-duration="1.5s" data-delay=".7s">Lorem ipsum
-                                    dolor sit amet, consetetur sadipscing elitr, sed dinonumy eirmod tempor invidunt
-                                    ut labore et dolore magna aliquyam erat. Lorem ipsum dolor sit amet, consetetur
-                                    sadipscing elitr.</p>
-                                <a href="#contact" class="btn theme-btn page-scroll" data-animation="fadeInUp" data-duration="1.5s"
-                                    data-delay=".9s">Prendre un rendez-vous</a>
+                                <h2 data-animation="fadeInDown" data-duration="1.5s" data-delay=".5s">
+                                    Moroccan Diabetes Society
+                                </h2>
+
+                                <h3 data-animation="fadeInDown" data-duration="1.5s" data-delay=".5s">
+                                    الجمعية المغربية لمرض السكري
+                                </h3>
+                            </div>
+
+                            <div>
+                                <img src="{{asset('assets/img/logo/logo.png')}}" alt="">
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="single-slider img-bg" style="background-image:url('assets/img/slider/slider-3.jpg');">
+            <div class="single-slider img-bg" style="background: rgb(188, 255, 188)">
                 <div class="container">
                     <div class="row">
-                        <div class="col-xl-7 col-lg-8 col-md-10">
+                        <div class="d-flex align-items-center justify-content-between w-100 col-xl-7 col-lg-8 col-md-10">
                             <div class="slider-content">
                                 <h1 data-animation="fadeInDown" data-duration="1.5s" data-delay=".5s">
-                                    {{config('app.name')}}
+                                    Société Morocaine
+                                    <br>
+                                    <b>de Diabète</b>
                                 </h1>
-                                <p data-animation="fadeInLeft" data-duration="1.5s" data-delay=".7s">Lorem ipsum
-                                    dolor sit amet, consetetur sadipscing elitr, sed dinonumy eirmod tempor invidunt
-                                    ut labore et dolore magna aliquyam erat. Lorem ipsum dolor sit amet, consetetur
-                                    sadipscing elitr.</p>
-                                <a href="#services" class="btn theme-btn page-scroll" data-animation="fadeInUp" data-duration="1.5s"
-                                    data-delay=".9s">Nos services</a>
+                                <h2 data-animation="fadeInDown" data-duration="1.5s" data-delay=".5s">
+                                    Moroccan Diabetes Society
+                                </h2>
+
+                                <h3 data-animation="fadeInDown" data-duration="1.5s" data-delay=".5s">
+                                    الجمعية المغربية لمرض السكري
+                                </h3>
+                            </div>
+
+                            <div>
+                                <img src="{{asset('assets/img/logo/logo.png')}}" alt="">
                             </div>
                         </div>
                     </div>
