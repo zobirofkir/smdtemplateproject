ba12230f597b2b28d95d96b4ae300bb32e913039
zobirofkir <zobirofkir19@gmail.com>
2024-11-22 19:00:18 +0100

    add custome style in ripple

--- public/assets/js/ripple.js
+++ public/assets/js/ripple.js
@@ -9,6 +9,13 @@ sliderWrapper.addEventListener('mousemove', function (e) {
     ripple.style.left = `${e.clientX - rect.left}px`;
     ripple.style.top = `${e.clientY - rect.top}px`;
 
+    const size = Math.random() * 20 + 10;
+    const opacity = Math.random() * 0.5 + 0.5;
+
+    ripple.style.width = `${size}px`;
+    ripple.style.height = `${size}px`;
+    ripple.style.opacity = opacity;
+
     sliderWrapper.appendChild(ripple);
 
     setTimeout(() => {
