80146afb1fd929efce6b328c05e2d2d2c8172ca6
zobirofkir <zobirofkir19@gmail.com>
2024-11-24 16:35:38 +0100

    update some needs

--- resources/views/components/videos.blade.php
+++ resources/views/components/videos.blade.php
@@ -16,15 +16,7 @@
                                 <h2>Journée de Printemps de Neurologie 2023</h2>
                                 <p>Mai 4, 5, 6 2023</p>
                                 <div class="social-links">
-                                    <a href="#" aria-label="LinkedIn">
-                                        <i class="fa-brands fa-linkedin fa-xl"></i>
-                                    </a>
-                                    <a href="#" aria-label="WhatsApp">
-                                        <i class="fa-brands fa-whatsapp fa-xl"></i>
-                                    </a>
-                                    <a href="#" aria-label="Facebook">
-                                        <i class="fa-brands fa-facebook fa-xl"></i>
-                                    </a>
+                                    <i class="fa-solid fa-play fa-2xl"></i>
                                 </div>
                             </div>
                         </a>
@@ -39,15 +31,7 @@
                                 <h2>Journée de Printemps de Neurologie 2023</h2>
                                 <p>Mai 4, 5, 6 2023</p>
                                 <div class="social-links">
-                                    <a href="#" aria-label="LinkedIn">
-                                        <i class="fa-brands fa-linkedin fa-xl"></i>
-                                    </a>
-                                    <a href="#" aria-label="WhatsApp">
-                                        <i class="fa-brands fa-whatsapp fa-xl"></i>
-                                    </a>
-                                    <a href="#" aria-label="Facebook">
-                                        <i class="fa-brands fa-facebook fa-xl"></i>
-                                    </a>
+                                    <i class="fa-solid fa-play fa-2xl"></i>
                                 </div>
                             </div>
                         </a>
@@ -62,6 +46,6 @@
 <div id="customModal" class="modal hidden">
     <div class="modal-content">
         <span id="closeModal" class="close">&times;</span>
-        <iframe id="videoFrame" width="560" height="315" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe id="videoFrame" style="border-radius: 20px;" width="560" height="315" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
 </div>
