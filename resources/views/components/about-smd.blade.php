23bc8ff47a1c4b816073dd232a4df84553c963c6
zobirofkir <zobirofkir19@gmail.com>
2024-12-04 22:39:00 +0100

    update

--- resources/views/components/about-smd.blade.php
+++ resources/views/components/about-smd.blade.php
@@ -1,16 +1,26 @@
 <section class="about-smd pt-120 pb-120">
     <div class="about-smd-container">
         <div class="image-container">
-            <img src="{{asset('assets/img/last-events/last-event-3.jpeg')}}" alt="Affiche" class="about-smd-image">
+            <img src="{{asset('assets/img/last-events/about-smd.jpeg')}}" alt="Affiche" class="about-smd-image">
         </div>
 
         <div class="content-container">
             <h1 class="about-smd-title">A Propos De Nous</h1>
             <h3 class="about-smd-subtitle">Société Morocaine de Diabétologie</h3>
             <p class="about-smd-description">
-                La Société Marocaine de Diabétologie (SMD) est une association à but non lucratif des diabétologues marocains,
-                dont le rôle est d’assurer la formation continue dans le domaine de la Diabétologie.
+                La Société Marocaine de Diabétologie (SMD), une association à but non lucratif, dédiée aux médecins spécialistes en diabétologie ou intéressés par cette discipline distinguée. Fondée dans le but de promouvoir une meilleure compréhension et gestion du diabète, la SMD joue un rôle crucial dans la formation continue des professionnels de la santé particulièrement au Maroc.
+
+                Notre association est fondée sur une vision de partage et d’échanges dans le domaine de la diabétologie, en adoptant une approche transversale et multidisciplinaire. La SMD s’engage à fournir des ressources éducatives de haute qualité, incluant des conférences, des séminaires, et des ateliers pratiques. Nous croyons fermement que l'union et la collaboration entre les différents spécialistes de la santé sont essentielles pour faire face aux défis posés par le diabète.
+
+                Parmi nos principales missions, nous nous attachons à :
             </p>
+            <ul class="about-smd-missions">
+                <li><strong>Assurer une formation continue et mise à jour pour les professionnels de la santé.</strong></li>
+                <li><strong>Promouvoir la recherche en diabétologie afin d’innover dans les méthodes de traitement.</strong></li>
+                <li><strong>Faciliter l’échange d’expertises et de connaissances entre les praticiens à travers des plateformes de communication et des événements dédiés.</strong></li>
+                <li><strong>Sensibiliser le grand public aux enjeux de la prévention et du traitement du diabète.</strong></li>
+                <li><strong>Fédérer les efforts des différents acteurs autour d'une vision commune du traitement et de la prise en charge du diabète.</strong></li>
+            </ul>
         </div>
     </div>
 </section>
