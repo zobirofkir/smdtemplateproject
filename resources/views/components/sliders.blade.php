c3099e2084a642404f93d172223023c8ec9fb7db
zobirofkir <zobirofkir19@gmail.com>
2024-12-05 18:19:08 +0100

    add video

--- resources/views/components/sliders.blade.php
+++ resources/views/components/sliders.blade.php
@@ -1,9 +1,13 @@
-
 <div class="slider-wrapper">
-
     <section class="slider-section">
         <div class="slider-active slick-style">
-            <div class="single-slider img-bg" style="background: #6dd6ff">
+            <div class="single-slider video-bg">
+                <!-- Video Background -->
+                <video autoplay loop muted playsinline class="slider-video">
+                    <source src="{{ asset('assets/videos/best-of-smd.mp4') }}" type="video/mp4">
+                    Your browser does not support the video tag.
+                </video>
+
                 <div class="container">
                     <div class="row">
                         <div class="d-flex align-items-center justify-content-between w-100 col-xl-7 col-lg-8 col-md-10">
@@ -19,97 +23,21 @@
                                 </h2>
 
                                 <h3 data-animation="fadeInDown" style="margin-top: 20px" data-duration="1.5s" data-delay=".5s" class="text-white">
-                                    الجمعية المغربية  لأمراض السكري
+                                    الجمعية المغربية لأمراض السكري
                                 </h3>
 
                                 <h4 data-animation="fadeInDown" data-duration="1.5s" style="margin-top: 20px" data-delay=".5s" class="text-white">
                                     ⵜⴰⵎⵙⵎⵓⵏⵜ ⵜⴰⵎⵔⵔⵓⴽⵉⵜ ⵏ ⵡⴰⵟⵟⴰⵏ ⵏ ⵙⵙⴽⴻⵕ
                                 </h4>
-
                             </div>
 
                             <div>
-                                <img src="{{asset('assets/img/logo/logo.png')}}" class="logo_right" alt="">
+                                <img src="{{ asset('assets/img/logo/logo.png') }}" class="logo_right" alt="">
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        <div class="single-slider img-bg"
-                style="background-image: url('{{ asset("assets/img/slider/slider-1.jpeg") }}'); background-size: cover; background-position: center;">
-            <div class="container">
-                <div class="row">
-                    <div class="d-flex align-items-center justify-content-between w-100 col-xl-7 col-lg-8 col-md-10">
-                        <div class="slider-content">
-                            <h1 data-animation="fadeInDown" data-duration="1.5s" data-delay=".5s">
-                                Société Morocaine
-                                <br>
-                                <b>de Diabétologie</b>
-                            </h1>
-
-                            <h2 data-animation="fadeInDown" data-duration="1.5s" data-delay=".5s" class="text-white">
-                                Moroccan Diabetology Society
-                            </h2>
-
-                            <h3 data-animation="fadeInDown" style="margin-top: 20px" data-duration="1.5s" data-delay=".5s" class="text-white">
-                                الجمعية المغربية  لأمراض السكري
-                            </h3>
-
-                            <h4 data-animation="fadeInDown" data-duration="1.5s" style="margin-top: 20px" data-delay=".5s" class="text-white">
-                                ⵜⴰⵎⵙⵎⵓⵏⵜ ⵜⴰⵎⵔⵔⵓⴽⵉⵜ ⵏ ⵡⴰⵟⵟⴰⵏ ⵏ ⵙⵙⴽⴻⵕ
-                            </h4>
-
-                        </div>
-
-                        <div>
-                            <img src="{{asset('assets/img/logo/logo.png')}}" class="logo_right" alt="">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="single-slider img-bg"
-                style="background-image: url('{{ asset("assets/img/slider/slider-2.jpeg") }}'); background-size: cover; background-position: center;">
-            <div class="container">
-                <div class="row">
-                    <div class="d-flex align-items-center justify-content-between w-100 col-xl-7 col-lg-8 col-md-10">
-                        <div class="slider-content">
-                            <h1 data-animation="fadeInDown" data-duration="1.5s" data-delay=".5s">
-                                Société Morocaine
-                                <br>
-                                <b>de Diabétologie</b>
-                            </h1>
-
-                            <h2 data-animation="fadeInDown" data-duration="1.5s" data-delay=".5s" class="text-white">
-                                Moroccan Diabetology Society
-                            </h2>
-
-                            <h3 data-animation="fadeInDown" style="margin-top: 20px" data-duration="1.5s" data-delay=".5s" class="text-white">
-                                الجمعية المغربية  لأمراض السكري
-                            </h3>
-
-                            <h4 data-animation="fadeInDown" data-duration="1.5s" style="margin-top: 20px" data-delay=".5s" class="text-white">
-                                ⵜⴰⵎⵙⵎⵓⵏⵜ ⵜⴰⵎⵔⵔⵓⴽⵉⵜ ⵏ ⵡⴰⵟⵟⴰⵏ ⵏ ⵙⵙⴽⴻⵕ
-                            </h4>
-
-                        </div>
-
-                        <div>
-                            <img src="{{asset('assets/img/logo/logo.png')}}" class="logo_right" alt="">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
         </div>
     </section>
-
 </div>
-
-
-
-
-
-
-
