37cdbc7640943e7c9ffab4c3f41cb6a6edc3b834
zobirofkir <zobirofkir19@gmail.com>
2024-12-06 10:46:29 +0100

    display event detail from hover

--- resources/views/components/events.blade.php
+++ resources/views/components/events.blade.php
@@ -1,25 +1,26 @@
 <section id="events" class="service-section pt-150 pb-150">
-    <div >
+    <div>
         <div class="mb-60">
             <h1 style="color: black; font-weight: bold; text-align: center;">Evénements</h1>
         </div>
         <div class="row">
-            @foreach ([1, 2, 3, 4, 5] as $key =>$event)
-            @php
-                $imagePath = public_path('assets/img/events/event-' . ($key + 1) . '.jpeg');
-            @endphp
-            @if (file_exists($imagePath))
-                <div class="col-md-4 mb-4">
-                    <div class="card shadow-lg rounded border-0 h-100">
-                        <img src="{{ asset('assets/img/events/event-' . ($key + 1) . '.jpeg') }}" alt="Event Image" class="card-img-top rounded-top">
-                        <div class="card-body">
-                            <h5 class="card-title event-title">Event {{ $event }}</h5>
-                            <a href="#" class="btn btn-primary">Learn More</a>
+            @foreach (range(1, 1000) as $key => $event)
+                @php
+                    $imagePath = public_path('assets/img/events/event-' . ($key + 1) . '.jpeg');
+                @endphp
+                @if (file_exists($imagePath))
+                    <div class="col-md-4 mb-4">
+                        <div class="card shadow-lg rounded border-0 h-100 position-relative">
+                            <img src="{{ asset('assets/img/events/event-' . ($key + 1) . '.jpeg') }}" alt="Event Image" class="card-img-top rounded-top">
+                            <!-- Event Info Overlay -->
+                            <div class="event-info-overlay">
+                                <h3 class="text-white">Event Info</h3>
+                                <p class="text-white">Event details or actions can go here.</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            @endif
-        @endforeach
+                @endif
+            @endforeach
         </div>
     </div>
 </section>
